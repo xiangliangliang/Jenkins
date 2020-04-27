fb3e0954f54f8a7c38b1c72d16b7c0e4bafec726
wenyan <284604666@qq.com>
2020-04-27 21:38:25 +0800

    b

--- log_parse.groovy
+++ log_parse.groovy
@@ -2,10 +2,16 @@ def log_parse(){
 	node('master'){
 		stage('log_parse'){
 			sleep 10
-			logParser projectRulePath: 'log_parse', showGraphs: true, useProjectRule: true
-		}
+			logParser projectRulePath: 'log_parse', showGraphs: true, useProjectRule: true		}
 	}
+}
 
+def log_parse_1(){
+	node('master'){
+		stage('log_parse'){
+			sleep 10
+			logParser projectRulePath: 'log_parse', showGraphs: true, useProjectRule: true		}
+	}
 }
 
 return this
